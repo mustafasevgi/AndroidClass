27fdf8d92235c3e7561137e05dd568f3b4dbfd20
Mohammad Almalkawi <mohammad.almalkawi@gmail.com>
2013-01-01 22:07:30 -0800

    Adds a second activity and defines an intent.

--- SimpleUI/src/me/almalkawi/simpleui/MainActivity.java
+++ SimpleUI/src/me/almalkawi/simpleui/MainActivity.java
@@ -1,19 +1,23 @@
 package me.almalkawi.simpleui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    public static final String EXTRA_MESSAGE = "SimpleUI_MESSAGE";
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
     }
 
     public void sendMessage(View view) {
-        final EditText editMessage = (EditText) findViewById(R.id.edit_message);
-        Toast.makeText(this, "You received: " + editMessage.getText(), Toast.LENGTH_SHORT).show();
+        final EditText editText = (EditText) findViewById(R.id.edit_message);
+        final Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
+        startActivity(intent);
     }
 }
