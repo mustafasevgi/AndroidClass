e251ac8947cb58418acc9663d64bcd3167cc2e78
Mohammad Almalkawi <mohammad.almalkawi@gmail.com>
2013-01-01 20:45:05 -0800

    Adds Spanish support to SimpleUI.

--- SimpleUI/src/me/almalkawi/simpleui/MainActivity.java
+++ SimpleUI/src/me/almalkawi/simpleui/MainActivity.java
@@ -2,10 +2,18 @@ package me.almalkawi.simpleui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    public void sendMessage(View view) {
+        final EditText editMessage = (EditText) findViewById(R.id.edit_message);
+        Toast.makeText(this, "You received: " + editMessage.getText(), Toast.LENGTH_SHORT).show();
     }
 }
